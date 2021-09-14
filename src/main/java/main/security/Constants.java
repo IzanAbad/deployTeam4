b269fc72254efc7a224d7673c6dd8223bbdebcc9
IzanAbad <82185323+IzanAbad@users.noreply.github.com>
2021-09-14 11:45:33 +0200

    update

--- src/main/java/main/security/Constants.java
+++ src/main/java/main/security/Constants.java
@@ -1,20 +1,70 @@
 package main.security;
 
-public class Constants {
+import static main.security.Constants.LOGIN_URL;
+import static main.security.Constants.REGISTER_URL;
+import static main.security.Constants.SEARCH_URL1;
+import static main.security.Constants.SEARCH_URL2;
+import static main.security.Constants.SEARCH_URL3;
 
-	// Spring Security
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-	public static final String LOGIN_URL = "/login";
-	public static final String SEARCH_URL1 = "/api/poblaciones/nombre/{nombre}";
-	public static final String SEARCH_URL2 = "/api/poblaciones/{nombre}/hoteles";
-	public static final String SEARCH_URL3 = "/api/hoteles/{id_hotel}";
-	public static final String HEADER_AUTHORIZACION_KEY = "Authorization";
-	public static final String TOKEN_BEARER_PREFIX = "Bearer ";
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
 
-	// JWT
+	private UserDetailsService userDetailsService;
 
-	public static final String ISSUER_INFO = "frank";
-	public static final String SUPER_SECRET_KEY = "1234";
-	public static final long TOKEN_EXPIRATION_TIME = 864_000_000; // 10 day
+	public WebSecurity(UserDetailsService userDetailsService) {
+		this.userDetailsService = userDetailsService;
+	}
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		/*
+		 * 1. Se desactiva el uso de cookies
+		 * 2. Se activa la configuración CORS con los valores por defecto
+		 * 3. Se desactiva el filtro CSRF
+		 * 4. Se indica que el login no requiere autenticación
+		 * 5. Se indica que el resto de URLs esten securizadas
+		 */
+		httpSecurity
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+			.cors().and()
+			.csrf().disable()
+			.authorizeRequests().antMatchers(HttpMethod.POST, LOGIN_URL, REGISTER_URL).permitAll()
+			.antMatchers(HttpMethod.GET, SEARCH_URL1, SEARCH_URL2, SEARCH_URL3).permitAll()
+			.anyRequest().authenticated().and()
+				.addFilter(new JWTAuthenticationFilter(authenticationManager()))
+				.addFilter(new JWTAuthorizationFilter(authenticationManager()));
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// Se define la clase que recupera los usuarios y el algoritmo para procesar las passwords
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
 }
